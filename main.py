d9a724e9e37ddc624d57c16fe9c300603caa32ad
Shayne Boyer <spboyer@live.com>
2021-11-11 22:34:11 +0000

    update to use PORT and uvicorn

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from typing import Optional, List
+from typing import List
+import os, uvicorn
+
+port = os.environ["PORT"]
 
 class Todo(BaseModel):
 
@@ -11,7 +14,6 @@ class Todo(BaseModel):
 app = FastAPI(title="Todo API")
 
 # Create, Read, Update, Delete
-
 store_todo = []
 
 @app.get('/')
@@ -31,11 +33,9 @@ async def get_all_todos():
 async def get_todo(id: int):
 
     try:
-        
         return store_todo[id]
     
     except:
-        
         raise HTTPException(status_code=404, detail="Todo Not Found")
     
 
@@ -43,12 +43,10 @@ async def get_todo(id: int):
 async def update_todo(id: int, todo: Todo):
 
     try:
-
         store_todo[id] = todo
         return store_todo[id]
     
     except:
-        
         raise HTTPException(status_code=404, detail="Todo Not Found")
 
 
@@ -56,11 +54,12 @@ async def update_todo(id: int, todo: Todo):
 async def delete_todo(id: int):
 
     try:
-
         obj = store_todo[id]
         store_todo.pop(id)
         return obj
     
     except:
-        
         raise HTTPException(status_code=404, detail="Todo Not Found")
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
